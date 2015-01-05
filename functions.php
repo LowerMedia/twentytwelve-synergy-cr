99ae886732ce99c42621770311aedea316b77f8e
hawkeye126 <pete.lower@gmail.com>
2015-01-05 13:44:27 -0800

    fix syntax

--- functions.php
+++ functions.php
@@ -590,7 +590,7 @@ function lowermedia_jquery_enqueue() {
     function load_fonts() {
         wp_dequeue_style( 'twentytwelve-fonts' );
         wp_deregister_style( 'twentytwelve-fonts' );
-        wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Signika:400,700|Open+Sans:400italic,700italic,400,700&amp;subset=latin,latin-ext');
+        wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Signika:400,700|Slabo+27px|Open+Sans:400italic,700italic,400,700&amp;subset=latin,latin-ext');
         wp_enqueue_style( 'googleFonts');
     }
     add_action('wp_print_styles', 'load_fonts');
