0e2173cdcc6e19e2ebba688ef65fb90cfe6c8f37
hawkeye126 <pete.lower@gmail.com>
2015-01-24 11:46:30 -0800

    add comments, remove unneeded debug code and extra lines

--- custom.js
+++ custom.js
@@ -1,24 +1,28 @@
+/**
+ *  Remove all reviews, except for one randomly chosen review
+ *
+ *
+ */
 
-
-jQuery(document).ready(function() {
-	jQuery('.hreview').show();
-});
-
-//ADD THE DASHICON SPAN AFTER THE DIV SO OUR :BEFORE STYLES WORK
 jQuery(function() {
-
-	
-
-	console.log(jQuery(".hreview").size());
+	//set var to random id by calculating the number of entries and randomly picking one number
 	var show_review_id = Math.floor((Math.random() * jQuery(".hreview").size()) + 1);
 	var count = 1;
 	jQuery(".hreview").each(function(){
-
+		//if the current id does not match the number, remove it
 		if( count !== show_review_id ){
 			jQuery(this).remove();
 		}
 		count++;
-
 	});
+});
 
+/**
+ *	Show hidden elements when the page has completed loading
+ *
+ *
+ */
+
+jQuery(document).ready(function() {
+	jQuery('.hreview').show();
 });
