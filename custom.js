f6686f01e5ddf2f304839ee826917afb3e65ad6d
hawkeye126 <pete.lower@gmail.com>
2015-02-10 09:46:41 -0800

    fix testimonials widget randomizer function"

--- custom.js
+++ custom.js
@@ -12,10 +12,13 @@ jQuery(function() {
 		console.log('this is the home page');
 	}
 
+	//Target sidebar testimonials
 	//set var to random id by calculating the number of entries and randomly picking one number
-	var show_review_id = Math.floor((Math.random() * jQuery(".hreview").size()) + 1);
+	var show_review_id = Math.floor((Math.random() * jQuery(".testimonial-wrap .hreview").size()) + 1);
 	var count = 1;
 	jQuery(".testimonial-wrap .hreview").each(function(){
+		console.log('Count: '+count);
+		console.log('Review ID: '+show_review_id);
 		//if the current id does not match the number, remove it
 		if( count !== show_review_id ){
 			jQuery(this).remove();
