5a1201170b0ddf19834837f7fc4f8d846c6a84c9
hawkeye126 <pete.lower@gmail.com>
2015-02-10 09:47:45 -0800

    remove unneeded code and comments

--- custom.js
+++ custom.js
@@ -5,13 +5,6 @@
  */
 
 jQuery(function() {
-	console.log('DocURL: '+document.URL);
-	console.log('WindowLocOrig: '+window.location.origin);
-	console.log('WindowLocHref: '+window.location.href);
-	if(window.location.origin+'/' === window.location.href){
-		console.log('this is the home page');
-	}
-
 	//Target sidebar testimonials
 	//set var to random id by calculating the number of entries and randomly picking one number
 	var show_review_id = Math.floor((Math.random() * jQuery(".testimonial-wrap .hreview").size()) + 1);
@@ -37,7 +30,3 @@ jQuery(function() {
 jQuery(document).ready(function() {
 	jQuery('.hreview').show();
 });
-
-// jQuery(document).ready(function() {
-// 	document.getElementsByTagName("html")[0].style.visibility = "visible";
-// });
