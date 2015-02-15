46856469f0f4c0b4213adb8ccbdb36235041bd20
hawkeye126 <pete.lower@gmail.com>
2015-02-15 10:02:38 -0800

    add front page blogroll output

--- content-page.php
+++ content-page.php
@@ -23,21 +23,23 @@
 					<div class='fpci-left fpci-div'><?php echo get_post_meta($post->ID, 'front_page_image_left', true); ?></div>
 					<div class='fpci-center fpci-div'><?php echo get_post_meta($post->ID, 'front_page_image_center', true); ?></div>
 					<div class='fpci-right fpci-div'><?php echo get_post_meta($post->ID, 'front_page_image_right', true); ?></div>
-				</div class='front-page-posts-holder'>
-
-				<h2><a href='/blog'>What's going on at Synergy?</a></h2>
-				<ul>
-				<?php
-					$args = array( 'numberposts' => 2 );
-
-					$recent_posts = wp_get_recent_posts( $args, ARRAY_A );
-					foreach( $recent_posts as $recent ){
-						echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
-					}
-				?>
-				</ul>
-				<div>
-				</div>
+				</div><!-- .front-page-custom-image-holder -->
+				<div class='front-page-posts-holder'>
+					<br />
+					<?php
+						$postslist = get_posts('numberposts=2&order=DESC&orderby=date');
+						foreach ( $postslist as $post ) :
+							setup_postdata( $post ); ?>
+							<div class="entry">
+								<h2><?php the_title(); ?></a></h2>
+								<?php the_time(get_option('date_format')) ?>
+								<?php the_excerpt(); ?>
+								<a href="<?php the_permalink(); ?>">Read More...</a>
+							</div>
+					<?php endforeach; ?>
+					<br />
+					<p><a href='/blog'>Click to read more about what's going on at Synergy!</a></p>
+				<div><!-- .front-page-posts-holder -->
 			<?php endif; ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
