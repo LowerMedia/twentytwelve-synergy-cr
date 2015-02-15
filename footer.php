38fef760d80c472a3e4908b78b07bf5e1a7c5f21
hawkeye126 <pete.lower@gmail.com>
2015-02-15 08:47:20 -0800

    update footer info add phonenumber shortcode

--- footer.php
+++ footer.php
@@ -11,10 +11,10 @@
 ?>
 
 	<?php if ( is_front_page() ) : ?>
-		<?php if ( is_active_sidebar( 'bottom-header-widget' ) ) : ?>
+		<?php if ( is_active_sidebar( 'bottom-content-widget' ) ) : ?>
 			<div id="bottom-content-widget-wrap" class="bottom-content-widget-wrap widget-area-wrap" role="complementary">
 				<?php dynamic_sidebar( 'bottom-content-widget' ); ?>
-			</div><!-- #bottom-header-widget -->
+			</div><!-- #bottom-content-widget -->
 		<?php endif; ?>
 	<?php endif; ?>
 	
@@ -24,19 +24,12 @@
 
 <footer id="colophon" role="contentinfo">
 	<div class="site-info">
-		<h4>Synergy Cedar Rapids - &copy;<?php echo date("Y"); ?></h4>
-		<h5><a href='http://lowermedia.net/'>A LowerMedia Site</a></h5>
+		<h4>Synergy Massage - &copy;<?php echo date("Y"); ?></h4>
+		<h5><a href='https://www.google.com/maps?q=329+10th+Ave.+SE+Ste+%23128+Cedar+Rapids,+IA&bav=on.2,or.r_qf.&bvm=bv.85970519,d.cGU&biw=1489&bih=948&dpr=1&um=1&ie=UTF-8&sa=X&ei=ZczgVMmAAY-rogT994GABg&ved=0CAYQ_AUoAQ'>329 10th Ave. SE Ste #128 Cedar Rapids, IA</a> | <?php echo do_shortcode( '[phonenumber]' ); ?></h5>
+		<h6><a href='http://lowermedia.net/'>A LowerMedia Site</a></h6>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 
-<?php wp_footer(); 
-
-/*
-
-<?php do_action( 'twentytwelve_credits' ); ?>
-		<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
-
-*/
-?>
+<?php wp_footer(); ?>
 </body>
 </html>
